1168d659ea366e23ecb1caa8c5bfce84f629ea1f
Faris Widyantho <radenfaris@gmail.com>
2017-12-07 13:37:14 +0700

    update home & navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <the-nav-bar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import TheNavBar from '@/components/TheNavBar'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TheNavBar
+  }
 }
 </script>
 
@@ -16,8 +20,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
